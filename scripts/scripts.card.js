4f4be6a68f876ae01c56b0e36e77f30594c777c1
Kosharti <artemka00913x@gmail.com>
2025-04-19 09:03:06 +0300

    перезалив проекта

---
+++ scripts/scripts.card.js
@@ -0,0 +1,13 @@
+fetch('https://jsonplaceholder.typicode.com/users/1')
+.then(response => response.json())
+.then(data => {
+  document.getElementById('profile-image').src = `./img/avatar_1.png`;
+  document.getElementById('profile-name').textContent = data.name;
+  document.getElementById('profile-title').textContent = 'Senior Product Designer';
+  document.getElementById('profile-location').textContent = `${data.address.city}, USA · Fulltime Freelancer`;
+  document.getElementById('about-me-text').textContent = 'Hi, I\'m a final year student completing a bachelor\'s In information Technology in QUT, with experience. We are the company behind the wildly successful DIY channel 5-Minute Crafts, the inspirational and creative channel Bright Side.';
+
+  document.getElementById('contact-email').textContent = "pristia@gmail.com";
+  document.getElementById('contact-phone').textContent = "0809021920139";
+})
+.catch(error => console.error('Error fetching user data:', error));
---
+++ scripts/scripts.js
@@ -0,0 +1,32 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.image-container .overlay');
+    let currentIndex = 0;
+    const slideInterval = 3000; 
+    let intervalId; 
+
+    function showImage(index) {
+        images.forEach((img, i) => {
+            img.style.display = (i === index) ? 'block' : 'none';
+        });
+    }
+
+    function startSlider() {
+        showImage(currentIndex); 
+        intervalId = setInterval(() => {
+            currentIndex = (currentIndex + 1) % images.length;
+            showImage(currentIndex);
+        }, slideInterval);
+    }
+
+    function handleImageError() {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    }
+
+    images.forEach((img) => {
+        img.addEventListener('error', handleImageError);
+    });
+
+    startSlider(); 
+});
